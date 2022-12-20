c7777c2d2fd2181799d322f27cea599cf9494c92
Sthefania Echeverry Orozco <sthefaniaecheverry@gmail.com>
2022-12-19 22:22:56 -0500

    configuración y estructura de un proyecto

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import {fileURLToPath} from 'url';
+import path from 'path';
+const app = express();
+
+// Habilita la lectura de datos del ejs
+// Actualizando el dirname para trabajar con modulos
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Configurar que la carpeta public es estática
+app.use(express.static('public'));
+
+// Habilitar vista con módulos y ejs
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
+// Configurar el puerto
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`servidor corriendo en el puerto ${port}`));
